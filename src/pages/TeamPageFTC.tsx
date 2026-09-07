9a477e51456f2738812a898fadb456c9d1cd562a
aarushmene <150198380+aarushmene@users.noreply.github.com>
2026-09-07 13:31:05 -0400

    Add new team member Arya Sinha to TeamPageFTC

--- src/pages/TeamPageFTC.tsx
+++ src/pages/TeamPageFTC.tsx
@@ -4,6 +4,7 @@ import PageLayout from "@/components/PageLayout";
 
 const members = [
   { name: "Aarush Mene", photo: "/assets/Aarush.png", grade: "7th", school: "Martin Middle", fll: "5 years (FLL-3,Explorer-2)" },
+  { name: "Arya Sinha", photo: "/assets/Arya.png", grade: "9th", school: "Wake STEM Early College", fll: "Rookie" },
   { name: "Aaryaman Raval", photo: "/assets/Aaryaman.png", grade: "10th", school: "Green Level High School", fll: "2 years" },
   { name: "Arjun Katta", photo: "/assets/ArjunKatta.png", grade: "7th", school: "TMSA", fll: "2 years" },
   { name: "Atharv Pardeshi", photo: "/assets/Atharv.png", grade: "9th", school: "Apex Friendship High School", fll: "1 Year" },
